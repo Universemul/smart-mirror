58407e32b33a72cd8d5290346f799cba3bcbd784
David <davidbarthelemy28@gmail.com>
2018-04-05 21:29:26 +0200

    remove periodic task

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import threading
 import time
 
 from flask import Flask, render_template, send_from_directory, jsonify
+from flask_socketio import SocketIO, emit, send
 
 from weather import Weather
 from rss import RssFluxUpdater
@@ -14,25 +15,17 @@ from rss import RssFluxUpdater
 # http://numberoverzero.com/posts/2017/07/17/periodic-execution-with-asyncio
 app = Flask(__name__, static_url_path='/static')
 rss_updater = RssFluxUpdater(limit=3)
-
 # region Misc
 
 def get_weather():
     weather = Weather().get_data()
     return weather
 
+@app.route('/rss')
 def periodic_rss():
-    print (rss_updater.fetch())
+    data = rss_updater.fetch()
+    return jsonify(data)
 
-@app.before_first_request
-def activate_job():
-    def run_job():
-        while True:
-            periodic_rss()
-            time.sleep(60 * 60 * 3) # 3 hours
-
-    thread = threading.Thread(target=run_job)
-    thread.start()
 # endregion
 
 # region Url
@@ -46,5 +39,4 @@ def home():
 @app.route('/<path:path>')
 def serve_static(path):
 	return send_from_directory('static', path)
-
 # endregion
